f01b4246211ed9132b25bcb67c308fea05d1a5fc
Craig Cornelius <cwcornelius@gmail.com>
2022-10-24 22:01:10 -0700

    Fix frequencies with lower case, avoiding non-letters

--- WordFrequencies.py
+++ WordFrequencies.py
@@ -13,13 +13,18 @@ class frequency():
     self.count_map = {}
     self.word_count = 0
     self.lowerCase = True
-
+    self.reSplit = re.compile("\W+")
+    
   def compute_map(self):
     for line in self.f:
-      words = line.split()
+#      words = line.split()
+      words = self.reSplit.split(line)
       for word in words:
         if self.lowerCase:
-          word = word.lower
+          word = word.lower()
+        word = word.strip()  # Remove
+        if not word:
+          continue
         if word not in self.count_map:
           self.count_map[word] = 1
         else:
@@ -35,12 +40,20 @@ class frequency():
       print(outline)
 
   def sort2(a, b):
+    if a[1] == b[1]:
+      # Consider alpha if same frequency
+      if a[0] < b[0]:
+        return 1
+      else:
+        return -1
+    # Frequencies only
     return b[1] - a[1]
   
   def output_tsv(self):
     keys = self.count_map.keys()
     total = float(self.word_count)
-    for result in sorted(self.count_map.items(), key=lambda item: item[1], reverse=True):
+    alpha_list = sorted(self.count_map.items(), key=lambda item: item[0])
+    for result in sorted(alpha_list, key=lambda item: item[1], reverse=True):
       outline = "%s\t%s" % (result[0], result[1])
       print(outline)
 
