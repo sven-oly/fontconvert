46fac6dd03498c8895e914804af6d0f1e5d75980
Craig Cornelius <cwcornelius@gmail.com>
2024-06-04 22:44:43 -0700

    XLS converter starting to work. Needs font

--- convertXls.py
+++ convertXls.py
@@ -3,6 +3,8 @@
 
 from __future__ import absolute_import, division, print_function
 
+from copy import copy
+
 import os
 import re
 import sys
@@ -31,7 +33,7 @@ class convertWorkbook():
         self.old_fonts = converter.oldFonts  # List of font names
         self.unicode_font = converter.unicodeFont
         self.cells_to_convert = ['c2', 'c3', 'c1987']
-        self.cell_ranges = [['c2','c1987']]
+        self.cell_ranges = [['c2', 'c1987']]
         self.workbook = None
 
         if self.input_path:
@@ -49,15 +51,27 @@ class convertWorkbook():
         sheets = self.workbook.sheetnames
         ws = self.workbook[sheets[0]]
 
+        self.converter.setScriptIndex(adlamConversion.LATIN2ADLAM)
         # Get the range to convert.
-        for cell in self.cells_to_convert:
-            latin = ws[cell].value
-            adlam = self.converter.convertText(latin, fontIndex=adlamConversion.LATIN2ADLAM)
-            ws[cell] = adlam
-
-        for range1 in self.cell_ranges[0]:
+        # for cell in self.cells_to_convert:
+        #     latin = ws[cell].value
+        #     adlam = self.converter.convertText(latin, fontIndex=adlamConversion.LATIN2ADLAM)
+        #     ws[cell] = adlam
+        # Special case
+        cells = []
+        for range1 in self.cell_ranges:
             cells = ws[range1[0]:range1[1]]
 
+        for cell in cells:
+            the_cell = cell[0]
+            latin = the_cell.value
+            adlam = self.converter.convertText(latin, fontIndex=adlamConversion.LATIN2ADLAM)
+            the_cell.value = adlam
+            # TODO: Set the font
+            old_font = the_cell.font
+            new_font = copy(old_font)
+            new_font.name = self.converter.defaultOutputFont
+            the_cell.font = new_font
 
     def processText(self):
         # Get the converter
@@ -97,6 +111,8 @@ def main(argv):
         if extension == '.xlsx':
             processor = convertWorkbook(path, args.output_dir, converter, debug_output)
             processor.process()
+            out_file_name = 'OUTPUT.xlsx'  # Temporary
+            processor.workbook.save(out_file_name)
         else:
             print('!!! Not processing file %s !' % path)
 
