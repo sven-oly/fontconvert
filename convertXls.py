baa07b0c2c64f449b8bba9b66063aa18cdc513bd
Craig Cornelius <cwcornelius@gmail.com>
2024-07-06 10:49:16 -0700

    Updated .xlsx converter to handle command line list of cells

--- convertXls.py
+++ convertXls.py
@@ -30,15 +30,16 @@ class cellData():
         self.text = ''
 
 class convertWorkbook():
-    def __init__(self, input_path, output_dir, converter, debug=False):
+    def __init__(self, input_path, output_dir, converter, debug=False, cell_ranges=None):
         self.oldFonts = []
         self.input_path = input_path
         self.output_dir = output_dir
         self.converter = converter
         self.old_fonts = converter.oldFonts  # List of font names
         self.unicode_font = converter.unicodeFont
-        self.cells_to_convert = ['c2', 'c3', 'c1987']
-        self.cell_ranges = [['c2', 'c1987']]
+
+        self.cell_ranges = cell_ranges
+
         self.workbook = None
 
         if self.input_path:
@@ -72,11 +73,15 @@ class convertWorkbook():
         # Special case
         cells = []
         for range1 in self.cell_ranges:
-            cells = ws[range1[0]:range1[1]]
+            this_range = ws[range1[0]:range1[1]]
+            cells.extend(this_range)
 
         for cell in cells:
             the_cell = cell[0]
             latin = the_cell.value
+            if not latin:
+                # Nothing there to process
+                continue
             adlam = self.converter.convertText(latin, fontIndex=adlamConversion.LATIN2ADLAM)
             the_cell.value = adlam
             # TODO: Set the font
@@ -107,11 +112,29 @@ class convertWorkbook():
         return
 
 
+def parse_cell_ranges(cell_string):
+    # Get list of cell ranges from comma separated list of cells and ranges
+    # also, ignore white space
+    split_ranges_pattern = re.compile(r'[\s\,\;]+')
+    rough_ranges = split_ranges_pattern.split(cell_string)
+    ranges = []
+    for g in rough_ranges:
+        if g.find(':') >= 0:
+            gs = g.split(':')
+            ranges.append(gs)
+        else:
+            ranges.append([g, g])
+    return ranges
+
 # For standalone and testing.
 def main(argv):
     global debug_output
 
     args = convertUtil.parseArgs()
+
+    # Ranges of cells, e.g., "c2:c100, d2, e3:g9]"
+    # Separated by comma, semicolon, or space
+    cell_ranges = parse_cell_ranges(args.cells)
 
     debug_output = True
     paths_to_doc = args.filenames
@@ -123,7 +146,8 @@ def main(argv):
         converter = adlamConversion.AdlamConverter()
         if extension == '.xlsx':
             out_file_name = converter.get_outfile_name(path)  # Temporary
-            processor = convertWorkbook(path, out_file_name, converter, debug_output)
+            processor = convertWorkbook(path, out_file_name, converter, debug_output, cell_ranges=cell_ranges)
+
             processor.process()  # Do the requested conversion
 
             processor.workbook.save(out_file_name)
